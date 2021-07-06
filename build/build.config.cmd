0f38c27154c92a7dfb88bb584de6b91448bf0e19
Grigore Stefan <g_stefan@yahoo.com>
2021-07-06 03:00:30 +0300

    update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-apache-httpd
-set PRODUCT_VERSION=2.4.46
+set PRODUCT_VERSION=2.4.48
 set PRODUCT_BASE=httpd
